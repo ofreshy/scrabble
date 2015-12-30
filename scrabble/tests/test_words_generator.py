e6aa686ed0b983cf3d06bbb50378875990140ec3
ofreshy <offer@magnetic.com>
2015-12-30 10:17:00 -0500

    Adding hands generator, sample code and non finished pattern generator

--- scrabble/tests/test_words_generator.py
+++ scrabble/tests/test_words_generator.py
@@ -1,5 +1,6 @@
 from unittest2 import TestCase
 
+from scrabble.constants import LETTERS
 from scrabble import words_generator
 
 
@@ -97,5 +98,72 @@ class TestSequenceGenerator(TestCase):
             # evaluate any line in the function!
             list(words_generator.generate_sequences("_,_,", 'AB'))
 
+
+class TestPatternsGenerator(TestCase):
+    def test_first_turn_hand(self):
+        expected = ['_', '_,_', '_,_,_', '_,_,_,_']
+        max_left, max_right, min_letters, max_letters = 7, 7, 1, 4
+        actual = list(
+            words_generator.generate_patterns(
+                '_',
+                max_left, max_right, min_letters, max_letters))
+
+        self.assertEquals(expected, actual)
+
+    def test_first_turn_hand_max_constraint(self):
+        expected = ['_', '_,_', '_,_,_']
+
+        # Since max left and right ! are 3 spaces
+        # Then the max constraint is 3
+        max_left, max_right, min_letters, max_letters = 3, 3, 1, 4
+        actual = list(
+            words_generator.generate_patterns(
+                '_',
+                max_left, max_right, min_letters, max_letters))
+
+        self.assertEquals(expected, actual)
+
+    def test_first_turn_hand_min_constraint(self):
+        expected = ['_,_,_']
+
+        # Since max left and right ! are 3 spaces
+        # Then the max constraint is actually 3
+        # as well as the min constraint
+        max_left, max_right, min_letters, max_letters = 3, 3, 4, 4
+        actual = list(
+            words_generator.generate_patterns(
+                '_',
+                max_left, max_right, min_letters, max_letters))
+
+        self.assertEquals(expected, actual)
+
+
+class TestHandsGenerator(TestCase):
+    def test_hand_without_jokers(self):
+        letters = 'ABCDEFG'
+        expected = [letters]
+
+        actual = list(words_generator.generate_hands(letters))
+
+        self.assertEquals(expected, actual)
+
+    def test_hand_single_joker(self):
+        letters = 'A*'
+        expected = [
+            'A{}'.format(l1) for l1 in LETTERS
+        ]
+
+        actual = list(words_generator.generate_hands(letters))
+
+        self.assertEquals(expected, actual)
+
+    def test_hand_two_jokers(self):
+        letters = '*A*'
+        expected = [
+            '{}A{}'.format(l1, l2) for l1 in LETTERS for l2 in LETTERS
+        ]
+
+        actual = list(words_generator.generate_hands(letters))
+        self.assertEquals(expected, actual)
 
 
--- scrabble/words_generator.py
+++ scrabble/words_generator.py
@@ -1,6 +1,7 @@
-from itertools import permutations
+from itertools import product, permutations
 import re
 
+from scrabble.constants import LETTERS
 
 PATTERN = re.compile(r'(\w,)*(\w)+$')
 
@@ -41,5 +42,58 @@ def generate_sequences(pattern, free_letters):
     for perm in perms:
         yield "".join(perm)
 
+
+def generate_patterns(fixed_letters, max_left=7, max_right=7, min_letters=1, max_letters=7):
+    """
+
+    :param str fixed_letters: such as M,_,E M,E etc.  _ means fixed but empty (middle slot)
+    :param int max_left: allowed spaces left from the last fixed letter
+    :param int max_right: allowed spaces right from the first fixed letter
+    :param int min_letters:
+    :param int max_letters:
+    :return:
+    """
+    if fixed_letters == '_':
+        # There are no letter constrains, so only constraints are spaces
+        real_max_letters = min(max(max_left, max_right), max_letters) + 1
+        real_min_letters = min(max(max_left, max_right), min_letters)
+        real_range = range(real_min_letters, real_max_letters)
+        patterns = (",".join(['_'] * r) for r in real_range)
+
+    for p in patterns:
+        yield p
+
+
+def generate_hands(letters):
+    num_of_jokers = letters.count('*')
+    if num_of_jokers:
+        combinations = product(LETTERS, repeat=num_of_jokers)
+        pattern = letters.replace('*', '{}')
+        hands = (pattern.format(*h) for h in combinations)
+    else:
+        hands = [letters]
+
+    for hand in hands:
+        yield hand
+
+
+def generate_words(dictionary, hands_generator, pattern_generator):
+    for hand in hands_generator:
+        for pattern in pattern_generator:
+            for sequence in generate_sequences(pattern, hand):
+                if sequence in dictionary:
+                    yield sequence
+
+if 1 == 0:
+    eligible_words = []
+    dictionary = None
+    free_letters = ''
+    fixed_letters = '_'
+
+    hands_generator = generate_hands(free_letters)
+    pattern_generator = generate_patterns(fixed_letters)
+    word_generator = generate_words(dictionary, hands_generator, pattern_generator)
+    # TODO - Instead, implement a deque with a value for each word
+    eligible_words = [w for w in word_generator]
 
 
